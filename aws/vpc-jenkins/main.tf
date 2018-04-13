7da8e5d4da8010687f46af704cb720086f34cc86
Shivin Vijai <shivinvijai@gmail.com>
2018-04-13 12:55:59 +0530

    Added variable.tf file for holding variables.

--- aws/vpc-jenkins/main.tf
+++ aws/vpc-jenkins/main.tf
@@ -1,17 +1,16 @@
 # Define AWS as our provider
 provider "aws" {
-  variable "access_key" = {}
-  variable "secret_key" = {}
-  variable "region" = {}
-  
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+
 }
 # Define our VPC
 resource "aws_vpc" "default" {
-  variable "cidr_block" = {}
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags {
     Name = "terraform-vpc"
   }
 }
-
