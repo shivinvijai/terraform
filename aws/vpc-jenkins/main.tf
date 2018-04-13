d8dc018076a4517568215ab28d13422064a590aa
Shivin Vijai <shivinvijai@gmail.com>
2018-04-13 11:09:06 +0530

    Variable issue fix

--- aws/vpc-jenkins/main.tf
+++ aws/vpc-jenkins/main.tf
@@ -1,13 +1,13 @@
 # Define AWS as our provider
 provider "aws" {
-  variable access_key = {}
-  variable secret_key = {}
-  variable region = {}
+  variable "access_key" = {}
+  variable "secret_key" = {}
+  variable "region" = {}
   
 }
 # Define our VPC
 resource "aws_vpc" "default" {
-  variable cidr_block = {}
+  variable "cidr_block" = {}
   enable_dns_hostnames = true
 
   tags {
